b33cacfbb4a7420cbab84fbb0c48dcb689b9503d
Victor Brown <victor.fbrown4@gmail.com>
2025-08-02 16:49:08 -0500

    editing front page a bit

--- src/App.tsx
+++ src/App.tsx
@@ -45,9 +45,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <div className="card">
         <input type="text" onChange={handleChange} onKeyDown={handleEnter} />
@@ -55,9 +52,6 @@ function App() {
       <div>
         <button onClick={validateInput}>Click me</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
